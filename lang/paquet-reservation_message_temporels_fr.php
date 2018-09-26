01728031ff20aa51283219655ae17e48bf9b6267
abelass <rainer@websimple.be>
2018-09-26 15:10:57 +0200

    Fabrique

---
+++ lang/paquet-reservation_message_temporels_fr.php
@@ -0,0 +1,14 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// R
+	'reservation_message_temporels_description' => 'Permet de définir depuis l\'espace privé, des messages qui seront envoyé à un moment donné, comme par exemple un rappel.',
+	'reservation_message_temporels_nom' => 'Réservation messages temporels',
+	'reservation_message_temporels_slogan' => 'Envoyé un message à un moment derminé',
+);
